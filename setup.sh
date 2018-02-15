b55d259289a997ce6f2d1a44a975e8a70d74fcf6
Oliver Marks <oly@digitaloctave.com>
2018-02-15 19:52:31 +0000

    New setup file, updated env example

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script to quickly startup a git and ci server with a pastebin for developmeny
+# Update the lines below with your domain, then setup your dns to be git.yourdomain and ci.yourdomain
+$MYDOMAIN=example.com
+$ADMINUSER=user
+$MYORG=example
+
+$GITSECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+$CISECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+mkdir -p /etc/nginx/sites-enabled/
+mkdir -p /var/www
+
+cd /var/www/
+git clone https://github.com/olymk2/project-hosting-setup.git
+
+cd /var/www/project-hosting-setup
+cp env.sample .env
+sed -i "s/exampleorg/${ADMINUSER}/g" .env
+sed -i "s/adminuser/${MYORG}/g" .env
+sed -i "s/example.com/${MYDOMAIN}/g" .env
+sed -i "s/giteapassword/${GITSECRET}/g" .env
+sed -i "s/drone-random-secret-here/${CISECRET}/g" .env
+
+sed -i "s/mydomain.com/${MYDOMAIN}/g" config/git.mydomain.com
+sed -i "s/mydomain.com/${MYDOMAIN}/g" config/ci.mydomain.com
+sed -i "s/mydomain.com/${MYDOMAIN}/g" config/pastebin.mydomain.com
+
+cp config/git.mydomain.com "/etc/nginx/sites-enabled/git.${MYDOMAIN}"
+cp config/ci.mydomain.com "/etc/nginx/sites-enabled/ci.${MYDOMAIN}"
+cp config/pastebin.mydomain.com "/etc/nginx/sites-enabled/pastebin.${MYDOMAIN}"
+
+docker-compose up -d
